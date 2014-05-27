207a2a2ebacb2e03d4ed9d179997dac9899124d0
Doug Coburn <dougco@microsoft.com>
2014-05-27 11:48:29 -0600

    updated compressor with streaming example

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <WinIoCtl.h>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include "zlib/deflate.h"
 
@@ -12,7 +13,9 @@
 std::string compute_hash(std::string str) {
     Sha256Digest digest;
     std::vector<byte> message(str.length());
-    memcpy(&message[0], &str[0], str.length());
+	if (str.length() > 0) {
+		memcpy(&message[0], &str[0], str.length());
+	}
     digest.update(message, 0, message.size());
     return digest.doFinal();
 }
@@ -37,53 +40,93 @@ void test_sha() {
 std::vector<char> deflate_message(std::string original_content) {
     z_stream z;
     memset(&z, 0, sizeof(z_stream));
-    char input_buffer[1024];
-    char output_buffer[1024];
-    int status;
-    /*
-    * Setup code for your specific problem
-    */
-    strncpy_s(input_buffer, original_content.c_str(), original_content.length());
-    z.next_in = (Bytef*)input_buffer;
-    z.avail_in = original_content.length();
-    z.next_out = (Bytef*)output_buffer;
-    z.avail_out = 1024;
-    deflateInit(&z, 9);
-    for (;;) {
-        if (z.avail_in == 0) {
-            break;
-        }
-        status = deflate(&z, Z_SYNC_FLUSH);
-    }
-    deflate(&z, Z_FINISH);
-    return std::vector<char>(output_buffer, output_buffer + (1024 - z.avail_out));
+    std::vector<char> input_buffer(1024);
+	std::vector<char> output_buffer(1024);
+	z.next_out = (Bytef*)output_buffer.data();
+	z.avail_out = output_buffer.size();
+	int status = deflateInit(&z, 9);
+	int flush = Z_NO_FLUSH;
+	std::vector<char> retval;
+
+	for (int offset = 0; offset < original_content.length(); offset += input_buffer.size()) {
+		z.avail_in = original_content.length() - offset;
+		if (input_buffer.size() < z.avail_in) {
+			z.avail_in = input_buffer.size();
+		}
+		z.next_in = (Bytef*)input_buffer.data();
+		memcpy(input_buffer.data(), &original_content[offset], z.avail_in);
+		while (z.avail_in) {
+			deflate(&z, flush);
+			if (z.avail_out == 0) {
+				retval.insert(end(retval), begin(output_buffer), end(output_buffer));
+				z.next_out = (Bytef*)output_buffer.data();
+				z.avail_out = output_buffer.size();
+			}
+ 		}
+	}
+	flush = Z_FINISH;
+	do {
+		deflate(&z, flush);
+		if (z.avail_out == 0) {
+			retval.insert(end(retval), begin(output_buffer), end(output_buffer));
+			z.next_out = (Bytef*)output_buffer.data();
+			z.avail_out = output_buffer.size();
+			continue;
+		}
+	} while (z.avail_out == 0);
+
+	if (z.avail_out != output_buffer.size()) {
+		retval.insert(end(retval), begin(output_buffer), end(output_buffer) - z.avail_out);
+		z.next_out = (Bytef*)output_buffer.data();
+		z.avail_out = output_buffer.size();
+	}
+	return retval;
 }
 
 std::string inflate_message(std::vector<char> compressed_content) {
-    z_stream z;
-    memset(&z, 0, sizeof(z_stream));
-    char input_buffer[1024];
-    char output_buffer[1024];
-    int status;
-    /*
-    * Setup code for your specific problem
-    */
-    strncpy_s(input_buffer, &compressed_content[0], compressed_content.size());
-    z.next_in = (Bytef*)input_buffer;
-    z.avail_in = compressed_content.size();
-    z.next_out = (Bytef*)output_buffer;
-    z.avail_out = 1024;
-    inflateInit(&z);
-    for (;;) {
-        if (z.avail_in == 0) {
-            break;
-        }
-        status = inflate(&z, Z_SYNC_FLUSH);
-        if (status != Z_OK) {
-            break;
-        }
-    }
-    return std::string(output_buffer, output_buffer + (1024 - z.avail_out));
+	z_stream z;
+	memset(&z, 0, sizeof(z_stream));
+	std::vector<char> input_buffer(1024);
+	std::vector<char> output_buffer(1024);
+	z.next_out = (Bytef*)output_buffer.data();
+	z.avail_out = output_buffer.size();
+	int status = inflateInit(&z, 9);
+	int flush = Z_NO_FLUSH;
+	std::string retval;
+
+	for (int offset = 0; offset < compressed_content.size(); offset += input_buffer.size()) {
+		z.avail_in = compressed_content.size() - offset;
+		z.next_in = (Bytef*)input_buffer.data();
+		if (input_buffer.size() < z.avail_in) {
+			z.avail_in = input_buffer.size();
+		}
+		memcpy(input_buffer.data(), &compressed_content[offset], z.avail_in);
+		while (z.avail_in) {
+			inflate(&z, flush);
+			if (z.avail_out == 0) {
+				retval.insert(end(retval), begin(output_buffer), end(output_buffer));
+				z.next_out = (Bytef*)output_buffer.data();
+				z.avail_out = output_buffer.size();
+			}
+		}
+	}
+	flush = Z_FINISH;
+	do {
+		inflate(&z, flush);
+		if (z.avail_out == 0) {
+			retval.insert(end(retval), begin(output_buffer), end(output_buffer));
+			z.next_out = (Bytef*)output_buffer.data();
+			z.avail_out = output_buffer.size();
+			continue;
+		}
+	} while (z.avail_out == 0);
+
+	if (z.avail_out != output_buffer.size()) {
+		retval.insert(end(retval), begin(output_buffer), end(output_buffer) - z.avail_out);
+		z.next_out = (Bytef*)output_buffer.data();
+		z.avail_out = output_buffer.size();
+	}
+	return retval;
 }
 
 void test_zlib() {
